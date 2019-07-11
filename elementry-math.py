4f243ddf3d1313a7600293c7a2fde39d97b1c4f1
Liora <lioralehrer@gmail.com>
2019-07-11 14:46:01 +0300

    application works :)

--- elementry-math.py
+++ elementry-math.py
@@ -3,17 +3,8 @@ import random
 
 no_answers = ["no", "NO", "n", "N", "No"]
 yes_answers = ["yes", "YES", "Yes", "Y", "y"]
-wrong_exercises = []
+wrong_answers = []
 correct_answers = []
-
-
-# def build_exercises_list():
-#     operators = ["+", "-", "*", "/"]
-#     exercise_list = []
-#     for i in range(11):
-#         exercise_list.append(str(random.randint(1, 20)) +
-#                              random.choice(operators) + str(random.randint(1, 11)))
-#     return exercise_list
 
 
 def start_play():
@@ -28,7 +19,6 @@ def start_play():
 def play():
     exercise = build_new_ex()
     answer = input(exercise+"= ")
-    print (exercise+answer)
     if eval(exercise) == eval(answer):
         correct_answers.append(exercise + "="+answer)
         ans = input("Correct!  Next exercise ?")
@@ -39,8 +29,22 @@ def play():
             if ans in yes_answers:
                 ans = None
                 return play()
-            else: ans = input("Do you want the next exercise ? insert yes or no  ")
-    else: print("Uncorrect!")            
+            else:
+                ans = input(
+                    "Do you want the next exercise ? insert yes or no  ")
+    else:
+        correct = eval(exercise)
+        wrong_answers.append(exercise+"="+answer+" ("+str(correct)+")")
+        ans = input(f"Uncorrect :( the answer is: {correct} \n Next exercise ?")
+        while ans != None:
+                if ans in no_answers:
+                    ans = None
+                    return results()
+                if ans in yes_answers:
+                    ans = None
+                    return play()
+                else:
+                    ans = input( "Do you want the next exercise ? insert yes or no  ")
 
 def build_new_ex():
     operators = ["+", "-", "*"]
@@ -48,8 +52,18 @@ def build_new_ex():
 
 
 def results():
-    # give the results of the play
-    print (correct_answers)
+    precent_correct = len(correct_answers) / (len(correct_answers)+len(wrong_answers))*100
+    if wrong_answers[0] != None:
+        print(f"“A list of problems answered wrong: ")
+        for i in wrong_answers:
+            print (i)
+    if correct_answers[0] != None:
+        print("Your correct answeres : ")
+        for i in correct_answers:
+            print (i)
+    print(
+        f"You have answered correctly {len(correct_answers)} out of {len(correct_answers)+len(wrong_answers)} problems ({precent_correct}%)")
     print("THE END")
+
 
 start_play()
